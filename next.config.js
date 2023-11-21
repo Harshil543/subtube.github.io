7c01fb4e69685d32071118c9888c9d9fa41f80d8
Hardik Anuvadiya <hardik.anuvadiya@gmail.com>
2023-11-21 19:45:49 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const path = require("path");
 const nextConfig = {
   trailingSlash: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, "styles")]
   },
   optimizeFonts: false,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
   env: {
     JWT_SECRET: "asdfghjklnbvcxzqwertyuiopmkioprewqasderfgnujm",
@@ -25,8 +25,8 @@ const nextConfig = {
       "https://api.cloudinary.com/v1_1/sub-tube/video/upload",
     CLOUDINARY_ZIP_URL: "https://api.cloudinary.com/v1_1/sub-tube/raw/upload",
     STRIPE_SECRET_KEY: "sk_test_2DqyjEwaU0Nq0PpEMVQ3qSAw00zgrbnfPk",
-    STRIPE_PUBLISHABLE_KEY: "pk_test_ZaZZWZGlvdIn12yFleIqyjSI00G4e18Kf7",
-  },
+    STRIPE_PUBLISHABLE_KEY: "pk_test_ZaZZWZGlvdIn12yFleIqyjSI00G4e18Kf7"
+  }
 };
 
 module.exports = nextConfig;
