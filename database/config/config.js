e12d06049fbe8a09aab1300ccf13a4bbce8ed570
Hardik Anuvadiya <hardik.anuvadiya@gmail.com>
2023-11-24 17:40:31 +0530

    Add module

--- database/config/config.js
+++ database/config/config.js
@@ -1,19 +1,21 @@
 export default {
-	development: {
-		username: process.env.NEXT_PUBLIC_DB_USERNAME,
-		password: process.env.NEXT_PUBLIC_DB_PASSWORD,
-		database: process.env.NEXT_PUBLIC_DB_NAME_DEVELOPMENT,
-		host: process.env.NEXT_PUBLIC_DB_HOSTNAME,
-		port: process.env.NEXT_PUBLIC_DB_PORT,
-		dialect: "mysql",
-	},
-	production: {
-		username: process.env.NEXT_PUBLIC_DB_USERNAME,
-		password: process.env.NEXT_PUBLIC_DB_PASSWORD_PROD,
-		database: process.env.NEXT_PUBLIC_DB_NAME_PRODUCTION,
-		host: process.env.NEXT_PUBLIC_DB_HOSTNAME,
-		port: process.env.NEXT_PUBLIC_DB_PORT_PROD,
-		logging: false,
-		dialect: "mysql",
-	},
+  development: {
+    username: process.env.NEXT_PUBLIC_DB_USERNAME,
+    password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+    database: process.env.NEXT_PUBLIC_DB_NAME_DEVELOPMENT,
+    host: process.env.NEXT_PUBLIC_DB_HOSTNAME,
+    port: process.env.NEXT_PUBLIC_DB_PORT,
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+  },
+  production: {
+    username: process.env.NEXT_PUBLIC_DB_USERNAME,
+    password: process.env.NEXT_PUBLIC_DB_PASSWORD_PROD,
+    database: process.env.NEXT_PUBLIC_DB_NAME_PRODUCTION,
+    host: process.env.NEXT_PUBLIC_DB_HOSTNAME,
+    port: process.env.NEXT_PUBLIC_DB_PORT_PROD,
+    logging: false,
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+  },
 };
